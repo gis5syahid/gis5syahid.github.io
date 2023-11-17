2805ab4c970baa5c6961d4a9f803101a95a62769
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-11-17 13:28:56 +0700

    update chapter 5

--- dashboard/js/template/geocf.js
+++ dashboard/js/template/geocf.js
@@ -1,6 +1,6 @@
 import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
 
-export let URLGeoJson = "https://asia-southeast2-msyahid.cloudfunctions.net/GetDataGeo";
+export let URLGeoJson = "https://asia-southeast2-msyahid.cloudfunctions.net/GetData";
 export let urlPostGCF = "https://asia-southeast2-msyahid.cloudfunctions.net/InsertDataGeo";
 export let UrlLogin = "https://asia-southeast2-msyahid.cloudfunctions.net/LoginGeo";
 export let UrlRegister = "https://asia-southeast2-msyahid.cloudfunctions.net/RegisterGeo";
